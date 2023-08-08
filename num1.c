40b50a6205c0115dd50271bfb7d4295c5cba6a68
KeyaRaniMondal <141219227+KeyaRaniMondal@users.noreply.github.com>
2023-08-08 07:42:52 -0700

    Add files via upload

---
+++ num1.c
@@ -0,0 +1,13 @@
+/*change two numbers without using third number*/
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    scanf("%d %d",&a,&b);
+    a=a+b;
+    b=a-b;
+    a=a-b;
+    printf("%d %d\n",a,b);
+    return 0;
+}
+
