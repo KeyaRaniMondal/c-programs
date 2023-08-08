3a2a80e4cf28c57b8fd68700195e9dd485fd071e
KeyaRaniMondal <141219227+KeyaRaniMondal@users.noreply.github.com>
2023-08-08 07:44:32 -0700

    Add files via upload

---
+++ q_r.c
@@ -0,0 +1,10 @@
+/*Finding quotient and remainder*/
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    scanf("%d %d",&a,&b);
+    printf("quotient=%d\n",a/b);
+    printf("remainder=%d\n",a%b);
+    return 0;
+}
